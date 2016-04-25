5256a8177a37f2fd425c4311d47770b6987d0e3e
Isaac Virshup <ivirshup@gmail.com>
2016-04-25 11:53:34 -0400

    Added ability to construct trees Module dependencies to FileTrees.jl

--- FileTrees.jl
+++ FileTrees.jl
@@ -38,12 +38,62 @@ function find_includes(file_path::AbstractString)
   return files
 end
 
-function plot_tree(g::DiGraph, files::Array, filename)
+function plot_tree(g::DiGraph, names::Array, filename)
   @assert !is_cyclic(g) "Your graph is cyclic"
-  layout_tree(g.fadjlist, file_names; filename=filename, cycles=false, ordering=:barycentric)
+  layout_tree(g.fadjlist, names; filename=filename, cycles=false, ordering=:barycentric)
 end
 
 function resolve_module() # This could get complicated to do statically. Look into doing it dynamically a la ThrowawayModules.jl?
 end
+
+"""Deals with dynamically finding what parts are used where"""
+function _module_tree(M::Module, d=OrderedDict{Module, Array{Module,1}}())
+    edges = find_modules(M)
+    d[M] = edges
+    for new_node in setdiff(edges, keys(d))
+        _module_tree(eval(M, new_node), d)
+    end
+    return d
+end
+
+"""Finds all modules used by the passing module"""
+function find_modules(M::Module)
+    ns = names(M, true)
+    modules = Module[]
+    for i in ns
+        e = try eval(M, i)
+        catch x
+            isa(x, UndefVarError) ? continue : rethrow(x)
+        end
+        if e === M # Don't allow self references
+            continue
+        elseif isa(e, Module)
+            push!(modules, e)
+        end
+    end
+    return modules
+end
+
+"""
+Creates a directed graph of module dependency.
+
+Uses dynamic analysis, so maybe some caution should be used.
+"""
+function module_tree(M::Module)
+    d = _module_tree(M)
+    node_names = map(module_name, keys(d))
+    g = DiGraph(length(node_names))
+    # edges
+    for (idx, v) in enumerate(values(d))
+        dests = findin(node_names, map(module_name, v))
+        for dest in dests
+            add_edge!(g, idx, dest)
+        end
+    end
+    is_cyclic(g) ? warn("Graph is cyclic!") : nothing
+    return g, node_names
+end
+
+
 
 end
